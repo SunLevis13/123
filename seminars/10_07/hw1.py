88706d7668f8fde7e3c41895d3fad491d46de956
Daria <dashazoueva@gmail.com>
2022-07-13 13:54:27 +0300

    hww

--- seminars/10_07/hw1.py
+++ seminars/10_07/hw1.py
@@ -1,33 +1,4 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-# def find_sum(x):
-#     s=0
-#     for i in range(1,x+1):
-#         s +=i
-#     print(f'Сумма цифр числа {num} = {s}')
-# num = int(input('Введите число: '))
-# find_sum(num)
-
-# num = int (input('Введите число: '))
-# ran = range(1,num+1)
-# numbers = list(ran)
-# print (numbers)
-# i = 0
-# sum = 0
-# while i < len(numbers):
-#     sum += numbers[i]
-#     i+=1
-# print (f'sum = {sum}')
-
-# num = int (input("Введите целое: "))
-# sum = 0
-# if num >= 1:
-#     sum = sum + num % 10
-#     num = num // 10
-#     print(sum)
-
-# print("Сумма цифр числа равна:", sum)
-
-
 def func(num):
     str_num = str(num)
     str_num = str_num.replace('.', '')  # 31415926
--- seminars/10_07/hw3.py
+++ seminars/10_07/hw3.py
@@ -1,36 +1,8 @@
 # Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 
-# def spisok (x):
-#     posled=0
-#     for i in range (1,x):
-#         posled = posled + pow((1+1/spisok[i]),spisok[i])
-#         print(posled)
-
-# spisok(n)
-        
-    # for i in posled:
-    #     posled.append( posled[pow((1+1/spisok[i]),spisok[i])] )
-#     #     i+=1
-#     return posled
-# print(spisok)
-
-
-# n1 = int(input('Введите число n1 '))
-# n2 = int(input('Введите число n2 '))
-# n3 = int(input('Введите число n3 '))
-# n4 = int(input('Введите число n4 '))
-# n5 = int(input('Введите число n5 '))
-
-# func [] = int [1,2,3,4,5]
-# new_f = []
-
-# for i in range(len(func)):
-#     new_f.append(func[pow((1+1/func[len(func)-i-1]),func[len(func)-i-1])])
-# print(new_f)
-
 def func(x):
     new_f = []
-    f = range(1,x) # 1 2 3 4
+    f = range(1,x+1) # 1 2 3 4
     spisok = list(f) # [1, 2, 3, 4]
     print(spisok) 
     sum = 0
