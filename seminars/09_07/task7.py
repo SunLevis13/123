988765ee56c9902294fc1d542bc69c623a3a1541
Daria <dashazoueva@gmail.com>
2022-07-17 15:59:04 +0300

    new

--- seminars/09_07/task7.py
+++
@@ -1,9 +0,0 @@
-# 7. Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
-# для всех значений предикат
-rezult = True
-for X in True, False:
-    for Y in True, False:
-        for Z in True, False:
-            # rezult = rezult and (not(X or Y or Z) == (not X and not Y and not Z))
-            print (f"({X =} {Y =} {Z =} RESULT: {not(X or Y or Z) == (not X and not Y and not Z)}")
-print(rezult)
