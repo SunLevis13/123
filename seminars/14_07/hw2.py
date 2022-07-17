8beb3b11b58343d7ae2430a0d8b60ca84e1650b8
Daria <dashazoueva@gmail.com>
2022-07-18 00:10:18 +0300

    hw2

---
+++ seminars/14_07/hw2.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+list = [2,3,4,5,6]
+list2 = []
+for i in range(len(list)):
+    if len(list)%2 == 0 and i < len(list)/2:
+        list2.append(list[i] * (list[len(list)-i-1]))
+    elif len(list)%2 != 0 and i < len(list)/2:
+        list2.append(list[i] * (list[len(list)-i-1]))
+
+        # else:
+        #  list2.append[list(len(list)/2+1)]
+print(list2)
