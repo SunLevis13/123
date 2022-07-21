88f215f33d279017fa30f597dfeaaff2daf80b27
Daria <dashazoueva@gmail.com>
2022-07-21 17:02:49 +0300

    hw3

---
+++ seminars/18_07/hw2.py
@@ -0,0 +1,22 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+import math
+
+def func(num): 
+    
+    while num % 2 == 0: 
+        print(2,) 
+        num = num / 2
+        
+    for i in range(3, int(math.sqrt(num)) + 1, 2): 
+
+        while num % i == 0: 
+            print(i,) 
+            num = num / i 
+    
+    if num > 2: 
+        print(int(num)) 
+
+num = int(input('Введите число: '))
+func(num) 
+
