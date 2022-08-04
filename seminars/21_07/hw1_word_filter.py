576c9bda9e39134f9ed4fe50fb6103916fa92b19
Daria <dashazoueva@gmail.com>
2022-08-04 13:01:02 +0300

    new

--- seminars/21_07/hw1_word_filter.py
+++ seminars/21_07/hw1_word_filter.py
@@ -11,15 +11,22 @@
 #     with open (f_name,'r') as f:
 #         return f.readline()
 
-# a = read_text('task3.txt').split()
-# b = ' '.join(filter(lambda x: 'абв' not in x,a))
+# a = read_text('hw1.txt').split()
+# b = ' '.join(filter(lambda x: 'abc' not in x,a))
 # print(b)
 
 # 3 способ через filter
-word = 'абв Ура, питон круто, очень интересно! абвг'
+
+import os
+path = os.path.join('folder21_07','hw1.txt')
+
+with open(path,'r') as data:
+    text = data.readline()
+print(text)
+
 def del_words(word):
-    word = list(filter(lambda x: 'абв' not in x,word.split()))
+    word = list(filter(lambda x: 'abc' not in x,word.split()))
     return ' '.join(word)
 
-word = del_words(word)
+word = del_words(text)
 print(word)
