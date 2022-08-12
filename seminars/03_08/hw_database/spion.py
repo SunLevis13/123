4ef2829ba411928eb48ec949f586e04f5c33eed0
Daria <dashazoueva@gmail.com>
2022-08-12 18:51:19 +0300

    sem

--- seminars/03_08/hw_database/spion.py
+++ seminars/03_08/hw_database/spion.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+import datetime
 
 def log(update: Update, context: CallbackContext):
     file = open('log_telbase.csv','a')
-    file.write(f'{update.effective_user.first_name},{update.effective_user.id},{update.message.text}\n')
+    file.write(f'date: {datetime.datetime.now().date()}, User: {update.effective_user.first_name}, id: {update.effective_user.id},{update.message.text}\n')
     file.close()
