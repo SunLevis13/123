da8e88e9279e8bc27b0413b0bfbfff471db85a8c
Daria <dashazoueva@gmail.com>
2022-08-15 17:33:17 +0300

    youtubedown

---
+++ seminars/04_08/youtube_downloader/main.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+from tkinter import *
+
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title('Youtube Downloader')
+
+Label(root, text = 'My Youtube Downloader', font = 'arial 15 bold').pack()
+
+link = StringVar()
+Label(root, text = 'Paste the video link: ', font = 'arial 15 bold').place(x=160,y=60)
+link_enter = Entry(root,width = 70, textvariable = link).place(x = 32, y = 90)
+
+def Downloader():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text = 'Success!', font = 'arial 15').place(x=210,y=210)
+    Label(root, text = f'Name video: {video.title} ', font = 'arial 15').place(x=20,y=260)
+
+
+Button(root, text = "Download", font = 'areal 15 bold', bg = 'turquoise2', padx = 2, command = Downloader).place(x = 195, y = 150)
+
+root.mainloop()
+
